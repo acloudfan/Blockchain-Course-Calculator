45d98b6547e33e75982d403f66e9e45bf305c39d
rajrin@yahoo.com <raj@acloudfan.com>
2017-05-11 19:39:20 -0400

    ok

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,5 @@
+var Calculator = artifacts.require("./Calculator.sol");
+
 module.exports = function(deployer) {
   deployer.deploy(Calculator);
 };
--- test/calculator.js
+++ test/calculator.js
@@ -1,41 +1,42 @@
-contract('Calculator', function (accounts) {
+var Calculator = artifacts.require("./Calculator.sol");
 
-  var account_0 = accounts[0];
-  var from = { from: account_0 }
+contract('Calculator', function(accounts) {
 
   // Expected behavior - result is initialized to 10
   // Test Case#1
-  it("should have a result=10 after the initialization", function () {
-    var calculator = Calculator.deployed();
-
-    return calculator.getResult.call().then(function (result) {
-        assert.equal(result.valueOf(), 10, "Contract initialized with value NOT equal to 10!!!");
+  it("should assert true", function() {
+    var calculator;
+    return Calculator.deployed().then(function(instance){
+      calculator = instance;
+      return calculator.getResult.call();
+    }).then(function (result) {
+      assert.equal(result.valueOf(), 10, "Contract initialized with value NOT equal to 10!!!");
     });
   });
-
 
   // Test case#2 Checks if calls to add/subtract for ((Initial_Value + 10) - 5) = 15
   it("should add 10 and then substract 5 to get a result=15", function () {
     // Get the deployed instance
-    var calculator = Calculator.deployed();
-
-    return calculator.addToNumber(10, from).then(function () {
+    var calculator;
+    
+    return Calculator.deployed().then(function(instance){
+      calculator = instance;
+      return calculator.addToNumber(10)
+    }).then(function () {
       return calculator.getResult.call();
     }).then(function (result) {
 
       assert.equal(result.toNumber(), 20, "Result after adding 10 should be 20");
       // Now substract 10
-      return calculator.substractNumber(5, from);
+      return calculator.substractNumber(5)
     }).then(function () {
       return calculator.getResult.call();
     }).then(function (result) {
       assert.equal(result.toNumber(), 15, "Result after subtracting 5 should be 15");
-
     });
   });
 
   // Add Test Case#3 here
 
   // Add Test Case#4 here
-
 });
