95e07263598950652f7df4ce4e543b70948d2fc9
acloudfan <raj@acloudfan.com>
2018-01-24 15:25:58 -0500

    comments

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
+// #1 Get an instance of the contract to be deployed/migrated
 var Calculator = artifacts.require("./Calculator.sol");
 
 module.exports = function(deployer) {
+  // #2 Deploy the instance of the contract
   deployer.deploy(Calculator);
 };
 
