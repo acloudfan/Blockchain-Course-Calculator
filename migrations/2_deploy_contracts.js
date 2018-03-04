7d898fa7f00b3c4aa0c085ddbe620f6010d0b8f5
acloudfan <raj@acloudfan.com>
2018-03-04 13:46:08 -0500

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ var Calculator = artifacts.require("./Calculator.sol");
 
 module.exports = function(deployer) {
   // #2 Deploy the instance of the contract
-  deployer.deploy(Calculator);
+  deployer.deploy(Calculator, 10);
 };
 
 
