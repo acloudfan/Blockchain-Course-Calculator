4d1f3bb149f6311ba10b9cfcf785beccf7401890
acloudfan <raj@acloudfan.com>
2018-03-18 21:29:40 -0400

    added a test case for CalculatorV2

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 // #1 Get an instance of the contract to be deployed/migrated
 var Calculator = artifacts.require("./Calculator.sol");
+var CalculatorV2 = artifacts.require("./CalculatorV2.sol");
 
 module.exports = function(deployer) {
   // #2 Deploy the instance of the contract
-  deployer.deploy(Calculator, 10);
+  deployer.deploy(Calculator);//, 10);
+  deployer.deploy(CalculatorV2, 10);
 };
 
 
